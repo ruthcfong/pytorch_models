2b005edab91a08825ea2644c0477dc3dc48a83f6
Ruth Fong <ruthfong@robots.ox.ac.uk>
2019-10-10 15:53:19 +0100

    update

--- models/self_supervised.py
+++ models/self_supervised.py
@@ -5,17 +5,38 @@ from torchvision.models.utils import load_state_dict_from_url
 
 
 AUDIO = 'weakly_audio'
-DEEP_CONTEXT = 'weakly_deepcontext'
+COLORIZATION = 'weakly_colorization'  # TODO(ruthfong): Add.
+DEEPCONTEXT = 'weakly_deepcontext'
 EGOMOTION = 'weakly_egomotion'
+MOVING = 'weakly_learningbymoving'
 OBJECTCENTRIC = 'weakly_objectcentric'
 PUZZLE = 'weakly_solvingpuzzle'
+SPLITBRAIN = 'weakly_splitbrain'  # TODO(ruthfong): Add.
+VIDEOORDER = 'weakly_videoorder'
+VIDEOTRACKING = 'weakly_videotracking'
+
+TASK_NAMES = [
+    AUDIO,
+    COLORIZATION,
+    DEEPCONTEXT,
+    EGOMOTION,
+    MOVING,
+    OBJECTCENTRIC,
+    PUZZLE,
+    SPLITBRAIN,
+    VIDEOORDER,
+    VIDEOTRACKING,
+]
 
 model_urls = {
-    AUDIO: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_audio-eb78354d.pt',
-    DEEP_CONTEXT: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_deepcontext-983efb8d.pt',
-    EGOMOTION: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_egomotion-a0a681d1.pt',
+    AUDIO: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_audio-eb78354d.pt',  # TODO(ruthfong): Debug backward.
+    DEEPCONTEXT: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_deepcontext-983efb8d.pt',
+    EGOMOTION: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_egomotion-a0a681d1.pt',  # TODO(ruthfong): Debug backward.
     OBJECTCENTRIC: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_objectcentric-95f20e7e.pt',
+    MOVING: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_learningbymoving-6326bbff.pt',
     PUZZLE: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_solvingpuzzle-51f91a86.pt',
+    VIDEOORDER: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_videoorder-883e5ce1.pt',
+    VIDEOTRACKING: 'https://github.com/ruthcfong/pytorch_models/releases/download/v2.0/alexnet-weakly_videotracking-0ba5696f31.pt',
 }
 
 
@@ -103,7 +124,7 @@ class AlexNet(nn.Module):
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
                              ceil_mode=True),
             ])
-        elif self.task_name == DEEP_CONTEXT:
+        elif self.task_name == DEEPCONTEXT:
             self.features = nn.Sequential(*[
                 nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4),
                           padding=(5, 5)),
@@ -181,56 +202,33 @@ class AlexNet(nn.Module):
                                momentum=0.999000012875, affine=False,
                                track_running_stats=True),
             ])
-            if False:
-                self.features = nn.Sequential(*[
-                    nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),
-                    nn.ReLU(inplace=True),
-                    nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
-                                 ceil_mode=True),
-                    nn.BatchNorm2d(96, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                    nn.LocalResponseNorm(5, alpha=1e-4, beta=0.75,
-                                         k=1.0),
-                    nn.Conv2d(96, 256, kernel_size=(5, 5), stride=(1, 1),
-                              padding=(2, 2), groups=2),
-                    nn.ReLU(inplace=True),
-                    nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
-                                 ceil_mode=True),
-                    nn.LocalResponseNorm(5, alpha=1e-4, beta=0.75,
-                                         k=1.0),
-                    nn.BatchNorm2d(256, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                    nn.Conv2d(256, 384, kernel_size=(3, 3), stride=(1, 1),
-                              padding=(1, 1)),
-                    nn.ReLU(inplace=True),
-                    nn.BatchNorm2d(384, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                    nn.Conv2d(384, 384, kernel_size=(3, 3), stride=(1, 1),
-                              padding=(1, 1), groups=2),
-                    nn.ReLU(inplace=True),
-                    nn.BatchNorm2d(384, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                    nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1),
-                              padding=(1, 1), groups=2),
-                    nn.ReLU(inplace=True),
-                    nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
-                                 ceil_mode=True),
-                    nn.BatchNorm2d(256, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                    Power(),
-                ])
-                self.classifier = nn.Sequential(*[
-                    nn.Linear(in_features=9216, out_features=4096, bias=True),
-                    nn.ReLU(inplace=True),
-                    nn.BatchNorm1d(4096, eps=1e-5,
-                                   momentum=0.999, affine=False,
-                                   track_running_stats=True),
-                ])
+        elif self.task_name == MOVING:
+            self.features = nn.Sequential(*[
+                nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=9.99999974738e-05, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(96, 256, kernel_size=(5, 5), stride=(1, 1),
+                          padding=(2, 2), groups=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=9.99999974738e-05, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(256, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1)),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+            ])
         elif self.task_name == OBJECTCENTRIC:
             self.features = nn.Sequential(*[
                 nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),
@@ -291,14 +289,78 @@ class AlexNet(nn.Module):
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
                              ceil_mode=True),
             ])
+        elif self.task_name == VIDEOORDER:
+            self.features = nn.Sequential(*[
+                nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=1e-04, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(96, 256, kernel_size=(5, 5), stride=(1, 1),
+                          padding=(2, 2), groups=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=1e-4, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(256, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1)),
+                nn.BatchNorm2d(384, eps=1e-5,
+                               momentum=0.999, affine=False,
+                               track_running_stats=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.BatchNorm2d(384, eps=1e-5,
+                               momentum=0.999, affine=False,
+                               track_running_stats=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.BatchNorm2d(256, eps=1e-5,
+                               momentum=0.999, affine=False,
+                               track_running_stats=True),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+            ])
+        elif self.task_name == VIDEOTRACKING:
+            self.features = nn.Sequential(*[
+                nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=1e-04, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(96, 256, kernel_size=(5, 5), stride=(1, 1),
+                          padding=(2, 2), groups=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+                nn.LocalResponseNorm(5, alpha=1e-04, beta=0.75,
+                                     k=1.0),
+                nn.Conv2d(256, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1)),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 384, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1),
+                          padding=(1, 1), groups=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1,
+                             ceil_mode=True),
+            ])
         else:
             raise NotImplementedError
 
 
-def alexnet(task_name=PUZZLE,
+def alexnet(task_name,
             pretrained=False,
             progress=True,
             **kwargs):
+    assert task_name in TASK_NAMES
     model = AlexNet(task_name=task_name, **kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[task_name],
